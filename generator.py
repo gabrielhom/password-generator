925d26bfa3a7b3bed1e82da368f8867e001b8f54
gabrielhom <ghom19@inf.ufpr.br>
2020-06-24 20:38:10 -0300

    format

--- generator.py
+++ generator.py
@@ -3,11 +3,16 @@ import string
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--size", required=False, type=int, default=16, help="password size")
-ap.add_argument("-l", "--lower", required=False, action='store_true', help="use lowercase letters")
-ap.add_argument("-u", "--upper", required=False, action='store_true', help="use uppercase letters")
-ap.add_argument("-d", "--digits", required=False, action='store_true', help="use digits")
-ap.add_argument("-p", "--punct", required=False, action='store_true', help="use punctuation")
+ap.add_argument("-s", "--size", required=False, type=int, default=16,
+                help="password size")
+ap.add_argument("-l", "--lower", required=False, action='store_true',
+                help="use lowercase letters")
+ap.add_argument("-u", "--upper", required=False, action='store_true',
+                help="use uppercase letters")
+ap.add_argument("-d", "--digits", required=False, action='store_true',
+                help="use digits")
+ap.add_argument("-p", "--punct", required=False, action='store_true',
+                help="use punctuation")
 args = ap.parse_args()
 
 characters = ""
