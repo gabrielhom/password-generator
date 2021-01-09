a34a09b0f0af8a0353ebf9d02a5c8a2b59aa5af3
gabrielhom <gabriel.henriqueomello@gmail.com>
2021-01-09 14:57:51 -0300

    code cleanup

--- generator.py
+++ generator.py
@@ -1,22 +1,25 @@
-from random import randint
-import string
-import argparse
+#!/usr/bin/env python3
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--size", required=False, type=int, default=16,
-                help="password size")
-ap.add_argument("-l", "--lower", required=False, action='store_true',
-                help="use lowercase letters")
-ap.add_argument("-u", "--upper", required=False, action='store_true',
-                help="use uppercase letters")
-ap.add_argument("-d", "--digits", required=False, action='store_true',
-                help="use digits")
-ap.add_argument("-p", "--punct", required=False, action='store_true',
-                help="use punctuation")
-args = ap.parse_args()
+from random import randint
+from argparse import ArgumentParser
+import string
+
+parser = ArgumentParser()
+parser.add_argument("-s", "--size", required=False, type=int, default=16,
+                    help="password size")
+parser.add_argument("-l", "--lower", required=False, action='store_true',
+                    help="use lowercase letters")
+parser.add_argument("-u", "--upper", required=False, action='store_true',
+                    help="use uppercase letters")
+parser.add_argument("-d", "--digits", required=False, action='store_true',
+                    help="use digits")
+parser.add_argument("-p", "--punct", required=False, action='store_true',
+                    help="use punctuation")
+args = parser.parse_args()
 
 characters = ""
 password = ""
+
 if args.lower:
     characters += string.ascii_lowercase
 if args.upper:
@@ -27,7 +30,7 @@ if args.punct:
     characters += string.punctuation
 
 if characters:
-    for i in range(0, args.size):
+    for x in range(args.size):
         password += characters[randint(0, len(characters) - 1)]
 
     print(password)
